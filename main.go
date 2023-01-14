06bc76b3af8b8cd22558881cc807687d7e3c3004
kuropenguin <kuro1994.t@gmail.com>
2023-01-14 17:49:00 +0900

    make main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		// Read the keyboad input.
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
